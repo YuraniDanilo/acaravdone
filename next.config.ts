146fb43e8f8e0400709f3e9bdec335c15c5e9702
YuraniDanilo <yuranidanilo.1@gmail.com>
2025-10-17 17:50:26 +0000

    modified:   next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,14 @@
-@type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+   eslint: {
+    ignoreDuringBuilds: true,
+  },
   typescript: {
     // 🚫 Ignora erros de tipo durante o build
     ignoreBuildErrors: true,
   },
-  eslint: {
-    // 🚫 Ignora erros do ESLint também
-    ignoreDuringBuilds: true,
-  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
